074c83b4d5168b09ebfc8027b25c599a1504e888
shubham <102307841+butola9730@users.noreply.github.com>
2022-07-11 11:21:48 +0530

    Create graph.py

---
+++ graph.py
@@ -0,0 +1,36 @@
+
+class Graph:
+
+    def __init__(self, edges, n):
+        
+        self.adjList = [[] for _ in range(n)]
+ 
+
+        for (src, dest) in edges:
+  
+            self.adjList[src].append(dest)
+ 
+ 
+
+def printGraph(graph):
+    for src in range(len(graph.adjList)):
+
+        for dest in graph.adjList[src]:
+            print(f'({src} —> {dest}) ', end='')
+        print()
+ 
+ 
+if __name__ == '__main__':
+ 
+
+    edges = [(0, 1), (1, 2), (2, 0), (2, 1), (3, 2), (4, 5), (5, 4)]
+ 
+
+    n = 6
+ 
+  
+    graph = Graph(edges, n)
+ 
+   
+    printGraph(graph)
+ 
